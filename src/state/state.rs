3505895e794ebee05ee367af70acc702f172369a
lsongzhi <lsongzhi@163.com>
2019-12-09 09:39:37 +0800

    Revert "refactor state machine pattern"
    This reverts commit 158416e0

--- src/state/state.rs
+++ src/state/state.rs
@@ -1,5 +1,5 @@
 pub trait State {
-    fn is_final() -> bool;
+    fn is_final(&self) -> bool;
 }
 
 state!(Monday);
--- src/state/state_machine.rs
+++ src/state/state_machine.rs
@@ -1,15 +1,25 @@
 use super::state::*;
-use std::marker::PhantomData;
 
 #[derive(Debug)]
 struct StateMachine<S: State> {
-    state: PhantomData<S>,
+    state: S,
 }
 
+
 impl<S: State> State for StateMachine<S> {
-    fn is_final() -> bool {
-        S::is_final()
+    fn is_final(&self) -> bool {
+        self.state.is_final()
     }
+}
+
+pub enum StateMachineWrapper {
+    Monday(StateMachine<Monday>),
+    Tuesday(StateMachine<Tuesday>),
+    Wednesday(StateMachine<Wednesday>),
+    Thursday(StateMachine<Thursday>),
+    Friday(StateMachine<Friday>),
+    Saturday(StateMachine<Saturday>),
+    Sunday(StateMachine<Sunday>),
 }
 
 edge!(Monday, Tuesday);
@@ -20,5 +30,18 @@ edge!(Friday, Saturday);
 edge!(Saturday, Sunday);
 edge!(Sunday, Monday);
 
+impl StateMachineWrapper {
+    pub fn next(self) -> Self {
+        match self {
+            StateMachineWrapper::Monday(s) => StateMachineWrapper::Tuesday(s.into()),
+            StateMachineWrapper::Tuesday(s) => StateMachineWrapper::Wednesday(s.into()),
+            StateMachineWrapper::Wednesday(s) => StateMachineWrapper::Thursday(s.into()),
+            StateMachineWrapper::Thursday(s) => StateMachineWrapper::Friday(s.into()),
+            StateMachineWrapper::Friday(s) => StateMachineWrapper::Saturday(s.into()),
+            StateMachineWrapper::Saturday(s) => StateMachineWrapper::Sunday(s.into()),
+            StateMachineWrapper::Sunday(s) => StateMachineWrapper::Monday(s.into())
+        }
+    }
+}
 
 
